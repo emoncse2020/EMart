2cb7a41585e12f8af24314da00b8189cc3fc9c09
emoncse2020 <emon.cse.2020@gmail.com>
2025-09-01 10:54:08 +0600

    three local apps are created

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local apps
+    'users',
+    'product',
+    'order',
+    'api',
 ]
 
 MIDDLEWARE = [
